0950c0239c77d3c38791d0ed31ba0673d5ff07d4
Jorge Giron <jorgegiron96@gmail.com>
2023-06-24 16:27:29 -0300

    teste

--- todos/urls.py
+++ todos/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name='todos'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:todo_id>/delete', views.delete, name='delete'),
     path('<int:todo_id>/update', views.update, name='update'),
-    path('add/', views.add, name='add')
+    path('add/', views.add, name='add'),
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
