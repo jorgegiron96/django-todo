07a2180a3173336669598a30093af2b595705a26
Jorge Giron <jorgegiron96@gmail.com>
2023-06-24 21:30:16 -0300

    change build

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,30 @@
 
-echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+# echo " BUILD START"
+# python3.9 -m pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "BUILD END"
+
+
+#!/bin/bash
+
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
--- todoApp/settings.py
+++ todoApp/settings.py
@@ -122,5 +122,11 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# Configures the staticfiles directory to serve
+# static files from /static/ on our deployment
+STATIC_ROOT = os.path.join(
+    BASE_DIR, 'staticfiles', 'static')
